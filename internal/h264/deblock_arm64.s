2511e8d8b7c3299404ef327cef6b1fe0e43ab05a
thesyncim <thesyncim@gmail.com>
2026-06-29 21:27:09 +0100

    Port arm64 luma deblock NEON

--- internal/h264/deblock_arm64.s
+++ internal/h264/deblock_arm64.s
@@ -3,360 +3,263 @@
 
 #include "textflag.h"
 
+// The luma deblock leaves below are raw arm64 encodings for the FFmpeg n8.0.1
+// NEON kernels ff_h264_v_loop_filter_luma_neon and
+// ff_h264_h_loop_filter_luma_neon. Go's arm64 assembler does not recognize
+// several required signed/saturating NEON mnemonics, so the instruction words
+// are emitted directly after the Go ABI arguments are moved into the same
+// x0..x4/w2..w3 registers used by the upstream ABI.
+
 // func h264VLoopFilterLuma8ASM(pix *uint8, stride int, alpha int32, beta int32, tc0 *int8)
 TEXT ·h264VLoopFilterLuma8ASM(SB), NOSPLIT, $0-32
-	MOVD pix+0(FP), R11
-	MOVD stride+8(FP), R12
-	MOVD $1, R13
-	MOVW alpha+16(FP), R14
-	MOVW beta+20(FP), R20
-	MOVD tc0+24(FP), R10
-	MOVD $4, R19
-	B    luma8_v_group
+	MOVD pix+0(FP), R0
+	MOVD stride+8(FP), R1
+	MOVW alpha+16(FP), R2
+	MOVW beta+20(FP), R3
+	MOVD tc0+24(FP), R4
 
-luma8_v_group:
-	MOVBU (R10), R16
-	CMPW  $128, R16
-	BLT   luma8_v_tc_ready
-	SUBW  $256, R16, R16
-luma8_v_tc_ready:
-	CMPW $0, R16
-	BGE  luma8_v_active
-	ADD  R13, R11, R11
-	ADD  R13, R11, R11
-	ADD  R13, R11, R11
-	ADD  R13, R11, R11
-	B    luma8_v_next_group
-luma8_v_active:
-	MOVD $4, R15
-luma8_v_inner:
-	SUB   R12, R11, R7
-	MOVBU (R7), R0
-	SUB   R12, R7, R7
-	MOVBU (R7), R1
-	SUB   R12, R7, R7
-	MOVBU (R7), R2
-	MOVBU (R11), R3
-	ADD   R12, R11, R7
-	MOVBU (R7), R4
-
-	SUBW R3, R0, R6
-	CMPW $0, R6
-	BGE  luma8_v_abs0_done
-	NEGW R6, R6
-luma8_v_abs0_done:
-	CMPW R14, R6
-	BGE  luma8_v_sample_done
-
-	SUBW R0, R1, R6
-	CMPW $0, R6
-	BGE  luma8_v_abs1_done
-	NEGW R6, R6
-luma8_v_abs1_done:
-	CMPW R20, R6
-	BGE  luma8_v_sample_done
-
-	SUBW R3, R4, R6
-	CMPW $0, R6
-	BGE  luma8_v_abs2_done
-	NEGW R6, R6
-luma8_v_abs2_done:
-	CMPW R20, R6
-	BGE  luma8_v_sample_done
-
-	MOVW R16, R17
-
-	SUBW R0, R2, R6
-	CMPW $0, R6
-	BGE  luma8_v_p_abs_done
-	NEGW R6, R6
-luma8_v_p_abs_done:
-	CMPW R20, R6
-	BGE  luma8_v_p_done
-	CMPW $0, R16
-	BEQ  luma8_v_p_inc
-	ADDW R3, R0, R6
-	ADDW $1, R6, R6
-	ASRW $1, R6, R6
-	ADDW R2, R6, R6
-	ASRW $1, R6, R6
-	SUBW R1, R6, R6
-	NEGW R16, R8
-	CMPW R8, R6
-	BGE  luma8_v_p_not_low
-	MOVW R8, R6
-luma8_v_p_not_low:
-	CMPW R16, R6
-	BLE  luma8_v_p_clip_done
-	MOVW R16, R6
-luma8_v_p_clip_done:
-	ADDW R1, R6, R6
-	SUB  R12, R11, R7
-	SUB  R12, R7, R7
-	MOVB R6, (R7)
-luma8_v_p_inc:
-	ADDW $1, R17, R17
-luma8_v_p_done:
-	ADD   R12, R11, R7
-	ADD   R12, R7, R7
-	MOVBU (R7), R5
-	SUBW  R3, R5, R6
-	CMPW  $0, R6
-	BGE   luma8_v_q_abs_done
-	NEGW  R6, R6
-luma8_v_q_abs_done:
-	CMPW R20, R6
-	BGE  luma8_v_delta
-	CMPW $0, R16
-	BEQ  luma8_v_q_inc
-	ADDW R3, R0, R6
-	ADDW $1, R6, R6
-	ASRW $1, R6, R6
-	ADDW R5, R6, R6
-	ASRW $1, R6, R6
-	SUBW R4, R6, R6
-	NEGW R16, R8
-	CMPW R8, R6
-	BGE  luma8_v_q_not_low
-	MOVW R8, R6
-luma8_v_q_not_low:
-	CMPW R16, R6
-	BLE  luma8_v_q_clip_done
-	MOVW R16, R6
-luma8_v_q_clip_done:
-	ADDW R4, R6, R6
-	ADD  R12, R11, R7
-	MOVB R6, (R7)
-luma8_v_q_inc:
-	ADDW $1, R17, R17
-
-luma8_v_delta:
-	SUBW R0, R3, R6
-	LSLW $2, R6, R6
-	SUBW R4, R1, R8
-	ADDW R8, R6, R6
-	ADDW $4, R6, R6
-	ASRW $3, R6, R6
-	NEGW R17, R8
-	CMPW R8, R6
-	BGE  luma8_v_delta_not_low
-	MOVW R8, R6
-luma8_v_delta_not_low:
-	CMPW R17, R6
-	BLE  luma8_v_delta_clip_done
-	MOVW R17, R6
-luma8_v_delta_clip_done:
-	ADDW R6, R0, R8
-	CMPW $0, R8
-	BGE  luma8_v_p0_nonnegative
-	MOVW ZR, R8
-	B    luma8_v_p0_clip_done
-luma8_v_p0_nonnegative:
-	CMPW $255, R8
-	BLE  luma8_v_p0_clip_done
-	MOVW $255, R8
-luma8_v_p0_clip_done:
-	SUB  R12, R11, R7
-	MOVB R8, (R7)
-
-	SUBW R6, R3, R8
-	CMPW $0, R8
-	BGE  luma8_v_q0_nonnegative
-	MOVW ZR, R8
-	B    luma8_v_q0_clip_done
-luma8_v_q0_nonnegative:
-	CMPW $255, R8
-	BLE  luma8_v_q0_clip_done
-	MOVW $255, R8
-luma8_v_q0_clip_done:
-	MOVB R8, (R11)
-
-luma8_v_sample_done:
-	ADD  R13, R11, R11
-	SUBS $1, R15, R15
-	BNE  luma8_v_inner
-
-luma8_v_next_group:
-	ADD  $1, R10, R10
-	SUBS $1, R19, R19
-	BNE  luma8_v_group
-	RET
+	WORD $0x7100005f // cmp w2, #0x0
+	WORD $0xb9400086 // ldr w6, [x4]
+	WORD $0x7a401860 // ccmp w3, #0x0, #0x0, ne
+	WORD $0x4e041cd8 // mov.s v24[0], w6
+	WORD $0x0a0640c8 // and w8, w6, w6, lsl #16
+	WORD $0x54000060 // b.eq +0x0c
+	WORD $0x6a082108 // ands w8, w8, w8, lsl #8
+	WORD $0x5400004a // b.ge +0x08
+	WORD $0xd65f03c0 // ret
+	WORD $0x4cc17000 // ld1.16b {v0}, [x0], x1
+	WORD $0x4cc17002 // ld1.16b {v2}, [x0], x1
+	WORD $0x4cc17004 // ld1.16b {v4}, [x0], x1
+	WORD $0xcb010800 // sub x0, x0, x1, lsl #2
+	WORD $0xcb010400 // sub x0, x0, x1, lsl #1
+	WORD $0x4cc17014 // ld1.16b {v20}, [x0], x1
+	WORD $0x4cc17012 // ld1.16b {v18}, [x0], x1
+	WORD $0x4cc17010 // ld1.16b {v16}, [x0], x1
+	WORD $0x4e010c56 // dup.16b v22, w2
+	WORD $0x2f08a718 // ushll.8h v24, v24, #0
+	WORD $0x6e207615 // uabd.16b v21, v16, v0
+	WORD $0x2f10a718 // ushll.4s v24, v24, #0
+	WORD $0x6e30765c // uabd.16b v28, v18, v16
+	WORD $0x6f185718 // sli.8h v24, v24, #8
+	WORD $0x6e20745e // uabd.16b v30, v2, v0
+	WORD $0x6f305718 // sli.4s v24, v24, #16
+	WORD $0x6e3536d5 // cmhi.16b v21, v22, v21
+	WORD $0x4e010c76 // dup.16b v22, w3
+	WORD $0x4e20ab17 // cmlt.16b v23, v24, #0
+	WORD $0x6e3c36dc // cmhi.16b v28, v22, v28
+	WORD $0x6e3e36de // cmhi.16b v30, v22, v30
+	WORD $0x4e771eb5 // bic.16b v21, v21, v23
+	WORD $0x6e307691 // uabd.16b v17, v20, v16
+	WORD $0x4e3c1eb5 // and.16b v21, v21, v28
+	WORD $0x6e207493 // uabd.16b v19, v4, v0
+	WORD $0x4e3e1eb5 // and.16b v21, v21, v30
+	WORD $0x0f0c86be // shrn.8b v30, v21, #4
+	WORD $0x4e083fc7 // mov.d x7, v30[0]
+	WORD $0x6e3136d1 // cmhi.16b v17, v22, v17
+	WORD $0x6e3336d3 // cmhi.16b v19, v22, v19
+	WORD $0xb4000667 // cbz x7, +0xcc
+	WORD $0x4e351e31 // and.16b v17, v17, v21
+	WORD $0x4e351e73 // and.16b v19, v19, v21
+	WORD $0x4e351f18 // and.16b v24, v24, v21
+	WORD $0x6e20161c // urhadd.16b v28, v16, v0
+	WORD $0x6e318715 // sub.16b v21, v24, v17
+	WORD $0x6e380e57 // uqadd.16b v23, v18, v24
+	WORD $0x6e3c0694 // uhadd.16b v20, v20, v28
+	WORD $0x6e3386b5 // sub.16b v21, v21, v19
+	WORD $0x6e3c049c // uhadd.16b v28, v4, v28
+	WORD $0x6e346ef7 // umin.16b v23, v23, v20
+	WORD $0x6e382e56 // uqsub.16b v22, v18, v24
+	WORD $0x6e380c44 // uqadd.16b v4, v2, v24
+	WORD $0x6e3666f7 // umax.16b v23, v23, v22
+	WORD $0x6e382c56 // uqsub.16b v22, v2, v24
+	WORD $0x6e3c6c9c // umin.16b v28, v4, v28
+	WORD $0x2f08a404 // ushll.8h v4, v0, #0
+	WORD $0x6e36679c // umax.16b v28, v28, v22
+	WORD $0x6f08a414 // ushll2.8h v20, v0, #0
+	WORD $0x2e303084 // usubw.8h v4, v4, v16
+	WORD $0x6e303294 // usubw2.8h v20, v20, v16
+	WORD $0x4f125484 // shl.8h v4, v4, #2
+	WORD $0x4f125694 // shl.8h v20, v20, #2
+	WORD $0x2e321084 // uaddw.8h v4, v4, v18
+	WORD $0x6e321294 // uaddw2.8h v20, v20, v18
+	WORD $0x2e223084 // usubw.8h v4, v4, v2
+	WORD $0x6e223294 // usubw2.8h v20, v20, v2
+	WORD $0x0f0d8c84 // rshrn.8b v4, v4, #3
+	WORD $0x4f0d8e84 // rshrn2.16b v4, v20, #3
+	WORD $0x6e721ef1 // bsl.16b v17, v23, v18
+	WORD $0x6e621f93 // bsl.16b v19, v28, v2
+	WORD $0x6e20bab7 // neg.16b v23, v21
+	WORD $0x2f08a61c // ushll.8h v28, v16, #0
+	WORD $0x4e356c84 // smin.16b v4, v4, v21
+	WORD $0x6f08a615 // ushll2.8h v21, v16, #0
+	WORD $0x4e376484 // smax.16b v4, v4, v23
+	WORD $0x2f08a416 // ushll.8h v22, v0, #0
+	WORD $0x6f08a418 // ushll2.8h v24, v0, #0
+	WORD $0x0e24139c // saddw.8h v28, v28, v4
+	WORD $0x4e2412b5 // saddw2.8h v21, v21, v4
+	WORD $0x0e2432d6 // ssubw.8h v22, v22, v4
+	WORD $0x4e243318 // ssubw2.8h v24, v24, v4
+	WORD $0x2e212b90 // sqxtun.8b v16, v28
+	WORD $0x6e212ab0 // sqxtun2.16b v16, v21
+	WORD $0x2e212ac0 // sqxtun.8b v0, v22
+	WORD $0x6e212b00 // sqxtun2.16b v0, v24
+	WORD $0xcb010400 // sub x0, x0, x1, lsl #1
+	WORD $0x4c817011 // st1.16b {v17}, [x0], x1
+	WORD $0x4c817010 // st1.16b {v16}, [x0], x1
+	WORD $0x4c817000 // st1.16b {v0}, [x0], x1
+	WORD $0x4c007013 // st1.16b {v19}, [x0]
+	WORD $0xd65f03c0 // ret
 
 // func h264HLoopFilterLuma8ASM(pix *uint8, stride int, alpha int32, beta int32, tc0 *int8)
 TEXT ·h264HLoopFilterLuma8ASM(SB), NOSPLIT, $0-32
-	MOVD pix+0(FP), R11
-	MOVD $1, R12
-	MOVD stride+8(FP), R13
-	MOVW alpha+16(FP), R14
-	MOVW beta+20(FP), R20
-	MOVD tc0+24(FP), R10
-	MOVD $4, R19
-	B    luma8_h_group
+	MOVD pix+0(FP), R0
+	MOVD stride+8(FP), R1
+	MOVW alpha+16(FP), R2
+	MOVW beta+20(FP), R3
+	MOVD tc0+24(FP), R4
 
-luma8_h_group:
-	MOVBU (R10), R16
-	CMPW  $128, R16
-	BLT   luma8_h_tc_ready
-	SUBW  $256, R16, R16
-luma8_h_tc_ready:
-	CMPW $0, R16
-	BGE  luma8_h_active
-	ADD  R13, R11, R11
-	ADD  R13, R11, R11
-	ADD  R13, R11, R11
-	ADD  R13, R11, R11
-	B    luma8_h_next_group
-luma8_h_active:
-	MOVD $4, R15
-luma8_h_inner:
-	SUB   R12, R11, R7
-	MOVBU (R7), R0
-	SUB   R12, R7, R7
-	MOVBU (R7), R1
-	SUB   R12, R7, R7
-	MOVBU (R7), R2
-	MOVBU (R11), R3
-	ADD   R12, R11, R7
-	MOVBU (R7), R4
-
-	SUBW R3, R0, R6
-	CMPW $0, R6
-	BGE  luma8_h_abs0_done
-	NEGW R6, R6
-luma8_h_abs0_done:
-	CMPW R14, R6
-	BGE  luma8_h_sample_done
-
-	SUBW R0, R1, R6
-	CMPW $0, R6
-	BGE  luma8_h_abs1_done
-	NEGW R6, R6
-luma8_h_abs1_done:
-	CMPW R20, R6
-	BGE  luma8_h_sample_done
-
-	SUBW R3, R4, R6
-	CMPW $0, R6
-	BGE  luma8_h_abs2_done
-	NEGW R6, R6
-luma8_h_abs2_done:
-	CMPW R20, R6
-	BGE  luma8_h_sample_done
-
-	MOVW R16, R17
-
-	SUBW R0, R2, R6
-	CMPW $0, R6
-	BGE  luma8_h_p_abs_done
-	NEGW R6, R6
-luma8_h_p_abs_done:
-	CMPW R20, R6
-	BGE  luma8_h_p_done
-	CMPW $0, R16
-	BEQ  luma8_h_p_inc
-	ADDW R3, R0, R6
-	ADDW $1, R6, R6
-	ASRW $1, R6, R6
-	ADDW R2, R6, R6
-	ASRW $1, R6, R6
-	SUBW R1, R6, R6
-	NEGW R16, R8
-	CMPW R8, R6
-	BGE  luma8_h_p_not_low
-	MOVW R8, R6
-luma8_h_p_not_low:
-	CMPW R16, R6
-	BLE  luma8_h_p_clip_done
-	MOVW R16, R6
-luma8_h_p_clip_done:
-	ADDW R1, R6, R6
-	SUB  R12, R11, R7
-	SUB  R12, R7, R7
-	MOVB R6, (R7)
-luma8_h_p_inc:
-	ADDW $1, R17, R17
-luma8_h_p_done:
-	ADD   R12, R11, R7
-	ADD   R12, R7, R7
-	MOVBU (R7), R5
-	SUBW  R3, R5, R6
-	CMPW  $0, R6
-	BGE   luma8_h_q_abs_done
-	NEGW  R6, R6
-luma8_h_q_abs_done:
-	CMPW R20, R6
-	BGE  luma8_h_delta
-	CMPW $0, R16
-	BEQ  luma8_h_q_inc
-	ADDW R3, R0, R6
-	ADDW $1, R6, R6
-	ASRW $1, R6, R6
-	ADDW R5, R6, R6
-	ASRW $1, R6, R6
-	SUBW R4, R6, R6
-	NEGW R16, R8
-	CMPW R8, R6
-	BGE  luma8_h_q_not_low
-	MOVW R8, R6
-luma8_h_q_not_low:
-	CMPW R16, R6
-	BLE  luma8_h_q_clip_done
-	MOVW R16, R6
-luma8_h_q_clip_done:
-	ADDW R4, R6, R6
-	ADD  R12, R11, R7
-	MOVB R6, (R7)
-luma8_h_q_inc:
-	ADDW $1, R17, R17
-
-luma8_h_delta:
-	SUBW R0, R3, R6
-	LSLW $2, R6, R6
-	SUBW R4, R1, R8
-	ADDW R8, R6, R6
-	ADDW $4, R6, R6
-	ASRW $3, R6, R6
-	NEGW R17, R8
-	CMPW R8, R6
-	BGE  luma8_h_delta_not_low
-	MOVW R8, R6
-luma8_h_delta_not_low:
-	CMPW R17, R6
-	BLE  luma8_h_delta_clip_done
-	MOVW R17, R6
-luma8_h_delta_clip_done:
-	ADDW R6, R0, R8
-	CMPW $0, R8
-	BGE  luma8_h_p0_nonnegative
-	MOVW ZR, R8
-	B    luma8_h_p0_clip_done
-luma8_h_p0_nonnegative:
-	CMPW $255, R8
-	BLE  luma8_h_p0_clip_done
-	MOVW $255, R8
-luma8_h_p0_clip_done:
-	SUB  R12, R11, R7
-	MOVB R8, (R7)
-
-	SUBW R6, R3, R8
-	CMPW $0, R8
-	BGE  luma8_h_q0_nonnegative
-	MOVW ZR, R8
-	B    luma8_h_q0_clip_done
-luma8_h_q0_nonnegative:
-	CMPW $255, R8
-	BLE  luma8_h_q0_clip_done
-	MOVW $255, R8
-luma8_h_q0_clip_done:
-	MOVB R8, (R11)
-
-luma8_h_sample_done:
-	ADD  R13, R11, R11
-	SUBS $1, R15, R15
-	BNE  luma8_h_inner
-
-luma8_h_next_group:
-	ADD  $1, R10, R10
-	SUBS $1, R19, R19
-	BNE  luma8_h_group
-	RET
+	WORD $0x7100005f // cmp w2, #0x0
+	WORD $0xb9400086 // ldr w6, [x4]
+	WORD $0x7a401860 // ccmp w3, #0x0, #0x0, ne
+	WORD $0x4e041cd8 // mov.s v24[0], w6
+	WORD $0x0a0640c8 // and w8, w6, w6, lsl #16
+	WORD $0x54000060 // b.eq +0x0c
+	WORD $0x6a082108 // ands w8, w8, w8, lsl #8
+	WORD $0x5400004a // b.ge +0x08
+	WORD $0xd65f03c0 // ret
+	WORD $0xd1001000 // sub x0, x0, #4
+	WORD $0x0cc17006 // ld1.8b {v6}, [x0], x1
+	WORD $0x0cc17014 // ld1.8b {v20}, [x0], x1
+	WORD $0x0cc17012 // ld1.8b {v18}, [x0], x1
+	WORD $0x0cc17010 // ld1.8b {v16}, [x0], x1
+	WORD $0x0cc17000 // ld1.8b {v0}, [x0], x1
+	WORD $0x0cc17002 // ld1.8b {v2}, [x0], x1
+	WORD $0x0cc17004 // ld1.8b {v4}, [x0], x1
+	WORD $0x0cc1701a // ld1.8b {v26}, [x0], x1
+	WORD $0x4dc18406 // ld1.d {v6}[1], [x0], x1
+	WORD $0x4dc18414 // ld1.d {v20}[1], [x0], x1
+	WORD $0x4dc18412 // ld1.d {v18}[1], [x0], x1
+	WORD $0x4dc18410 // ld1.d {v16}[1], [x0], x1
+	WORD $0x4dc18400 // ld1.d {v0}[1], [x0], x1
+	WORD $0x4dc18402 // ld1.d {v2}[1], [x0], x1
+	WORD $0x4dc18404 // ld1.d {v4}[1], [x0], x1
+	WORD $0x4dc1841a // ld1.d {v26}[1], [x0], x1
+	WORD $0x4e1428d5 // trn1.16b v21, v6, v20
+	WORD $0x4e1468d7 // trn2.16b v23, v6, v20
+	WORD $0x4e102a54 // trn1.16b v20, v18, v16
+	WORD $0x4e106a50 // trn2.16b v16, v18, v16
+	WORD $0x4e022806 // trn1.16b v6, v0, v2
+	WORD $0x4e026802 // trn2.16b v2, v0, v2
+	WORD $0x4e1a2892 // trn1.16b v18, v4, v26
+	WORD $0x4e1a689a // trn2.16b v26, v4, v26
+	WORD $0x4e5228c0 // trn1.8h v0, v6, v18
+	WORD $0x4e5268d2 // trn2.8h v18, v6, v18
+	WORD $0x4e5a2844 // trn1.8h v4, v2, v26
+	WORD $0x4e5a685a // trn2.8h v26, v2, v26
+	WORD $0x4e502ae2 // trn1.8h v2, v23, v16
+	WORD $0x4e506af7 // trn2.8h v23, v23, v16
+	WORD $0x4e542ab0 // trn1.8h v16, v21, v20
+	WORD $0x4e546ab5 // trn2.8h v21, v21, v20
+	WORD $0x4e802a06 // trn1.4s v6, v16, v0
+	WORD $0x4e806a00 // trn2.4s v0, v16, v0
+	WORD $0x4e842854 // trn1.4s v20, v2, v4
+	WORD $0x4e846842 // trn2.4s v2, v2, v4
+	WORD $0x4e926aa4 // trn2.4s v4, v21, v18
+	WORD $0x4e922ab2 // trn1.4s v18, v21, v18
+	WORD $0x4e9a2af0 // trn1.4s v16, v23, v26
+	WORD $0x4e9a6afa // trn2.4s v26, v23, v26
+	WORD $0x4e010c56 // dup.16b v22, w2
+	WORD $0x2f08a718 // ushll.8h v24, v24, #0
+	WORD $0x6e207615 // uabd.16b v21, v16, v0
+	WORD $0x2f10a718 // ushll.4s v24, v24, #0
+	WORD $0x6e30765c // uabd.16b v28, v18, v16
+	WORD $0x6f185718 // sli.8h v24, v24, #8
+	WORD $0x6e20745e // uabd.16b v30, v2, v0
+	WORD $0x6f305718 // sli.4s v24, v24, #16
+	WORD $0x6e3536d5 // cmhi.16b v21, v22, v21
+	WORD $0x4e010c76 // dup.16b v22, w3
+	WORD $0x4e20ab17 // cmlt.16b v23, v24, #0
+	WORD $0x6e3c36dc // cmhi.16b v28, v22, v28
+	WORD $0x6e3e36de // cmhi.16b v30, v22, v30
+	WORD $0x4e771eb5 // bic.16b v21, v21, v23
+	WORD $0x6e307691 // uabd.16b v17, v20, v16
+	WORD $0x4e3c1eb5 // and.16b v21, v21, v28
+	WORD $0x6e207493 // uabd.16b v19, v4, v0
+	WORD $0x4e3e1eb5 // and.16b v21, v21, v30
+	WORD $0x0f0c86be // shrn.8b v30, v21, #4
+	WORD $0x4e083fc7 // mov.d x7, v30[0]
+	WORD $0x6e3136d1 // cmhi.16b v17, v22, v17
+	WORD $0x6e3336d3 // cmhi.16b v19, v22, v19
+	WORD $0xb4000907 // cbz x7, +0x120
+	WORD $0x4e351e31 // and.16b v17, v17, v21
+	WORD $0x4e351e73 // and.16b v19, v19, v21
+	WORD $0x4e351f18 // and.16b v24, v24, v21
+	WORD $0x6e20161c // urhadd.16b v28, v16, v0
+	WORD $0x6e318715 // sub.16b v21, v24, v17
+	WORD $0x6e380e57 // uqadd.16b v23, v18, v24
+	WORD $0x6e3c0694 // uhadd.16b v20, v20, v28
+	WORD $0x6e3386b5 // sub.16b v21, v21, v19
+	WORD $0x6e3c049c // uhadd.16b v28, v4, v28
+	WORD $0x6e346ef7 // umin.16b v23, v23, v20
+	WORD $0x6e382e56 // uqsub.16b v22, v18, v24
+	WORD $0x6e380c44 // uqadd.16b v4, v2, v24
+	WORD $0x6e3666f7 // umax.16b v23, v23, v22
+	WORD $0x6e382c56 // uqsub.16b v22, v2, v24
+	WORD $0x6e3c6c9c // umin.16b v28, v4, v28
+	WORD $0x2f08a404 // ushll.8h v4, v0, #0
+	WORD $0x6e36679c // umax.16b v28, v28, v22
+	WORD $0x6f08a414 // ushll2.8h v20, v0, #0
+	WORD $0x2e303084 // usubw.8h v4, v4, v16
+	WORD $0x6e303294 // usubw2.8h v20, v20, v16
+	WORD $0x4f125484 // shl.8h v4, v4, #2
+	WORD $0x4f125694 // shl.8h v20, v20, #2
+	WORD $0x2e321084 // uaddw.8h v4, v4, v18
+	WORD $0x6e321294 // uaddw2.8h v20, v20, v18
+	WORD $0x2e223084 // usubw.8h v4, v4, v2
+	WORD $0x6e223294 // usubw2.8h v20, v20, v2
+	WORD $0x0f0d8c84 // rshrn.8b v4, v4, #3
+	WORD $0x4f0d8e84 // rshrn2.16b v4, v20, #3
+	WORD $0x6e721ef1 // bsl.16b v17, v23, v18
+	WORD $0x6e621f93 // bsl.16b v19, v28, v2
+	WORD $0x6e20bab7 // neg.16b v23, v21
+	WORD $0x2f08a61c // ushll.8h v28, v16, #0
+	WORD $0x4e356c84 // smin.16b v4, v4, v21
+	WORD $0x6f08a615 // ushll2.8h v21, v16, #0
+	WORD $0x4e376484 // smax.16b v4, v4, v23
+	WORD $0x2f08a416 // ushll.8h v22, v0, #0
+	WORD $0x6f08a418 // ushll2.8h v24, v0, #0
+	WORD $0x0e24139c // saddw.8h v28, v28, v4
+	WORD $0x4e2412b5 // saddw2.8h v21, v21, v4
+	WORD $0x0e2432d6 // ssubw.8h v22, v22, v4
+	WORD $0x4e243318 // ssubw2.8h v24, v24, v4
+	WORD $0x2e212b90 // sqxtun.8b v16, v28
+	WORD $0x6e212ab0 // sqxtun2.16b v16, v21
+	WORD $0x2e212ac0 // sqxtun.8b v0, v22
+	WORD $0x6e212b00 // sqxtun2.16b v0, v24
+	WORD $0x4e102a35 // trn1.16b v21, v17, v16
+	WORD $0x4e106a37 // trn2.16b v23, v17, v16
+	WORD $0x4e132819 // trn1.16b v25, v0, v19
+	WORD $0x4e13681b // trn2.16b v27, v0, v19
+	WORD $0x4e592ab1 // trn1.8h v17, v21, v25
+	WORD $0x4e596aa0 // trn2.8h v0, v21, v25
+	WORD $0x4e5b2af0 // trn1.8h v16, v23, v27
+	WORD $0x4e5b6af3 // trn2.8h v19, v23, v27
+	WORD $0xcb011000 // sub x0, x0, x1, lsl #4
+	WORD $0x91000800 // add x0, x0, #2
+	WORD $0x0d818011 // st1.s {v17}[0], [x0], x1
+	WORD $0x0d818010 // st1.s {v16}[0], [x0], x1
+	WORD $0x0d818000 // st1.s {v0}[0], [x0], x1
+	WORD $0x0d818013 // st1.s {v19}[0], [x0], x1
+	WORD $0x0d819011 // st1.s {v17}[1], [x0], x1
+	WORD $0x0d819010 // st1.s {v16}[1], [x0], x1
+	WORD $0x0d819000 // st1.s {v0}[1], [x0], x1
+	WORD $0x0d819013 // st1.s {v19}[1], [x0], x1
+	WORD $0x4d818011 // st1.s {v17}[2], [x0], x1
+	WORD $0x4d818010 // st1.s {v16}[2], [x0], x1
+	WORD $0x4d818000 // st1.s {v0}[2], [x0], x1
+	WORD $0x4d818013 // st1.s {v19}[2], [x0], x1
+	WORD $0x4d819011 // st1.s {v17}[3], [x0], x1
+	WORD $0x4d819010 // st1.s {v16}[3], [x0], x1
+	WORD $0x4d819000 // st1.s {v0}[3], [x0], x1
+	WORD $0x4d819013 // st1.s {v19}[3], [x0], x1
+	WORD $0xd65f03c0 // ret
